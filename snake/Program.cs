94830e6d8852bd6bbc860eaa453237c6fbd7961a
Sashok <1800brokenglass@gmail.com>
2020-04-11 11:57:10 +0300

    Added the first function DRAW

--- snake/Program.cs
+++ snake/Program.cs
@@ -6,7 +6,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Started the SNAKE project");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            draw(x1, y1, sym1);
+            draw(x2, y2, sym2);
+            Console.ReadLine();
+        }
+        static void draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
