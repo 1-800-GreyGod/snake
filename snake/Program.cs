5c92c908f23a0ce76bbeeeb3256fa28987aefe30
Sashok <1800brokenglass@gmail.com>
2020-04-11 12:39:49 +0300

    Added a constructor
    Created a constructor for the Point class

--- snake/Program.cs
+++ snake/Program.cs
@@ -6,15 +6,9 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            var p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            var p1 = new Point(1,3,'*');
             p1.draw();
-            var p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            var p2 = new Point(4,5,'#');
             p2.draw();
 
         }
