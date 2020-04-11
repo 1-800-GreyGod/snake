82de19bfe656ab65c2709e9c2624215306a32a0d
Sashok <1800brokenglass@gmail.com>
2020-04-11 13:14:54 +0300

    Added the HorizontalLine and the VerticalLine classes

--- snake/Program.cs
+++ snake/Program.cs
@@ -15,7 +15,13 @@ namespace snake
             pList.Add(p1);
             pList.Add(p2);
 
+            
+
+            HorizontalLine hline = new HorizontalLine(5,10,8,'+');
+            hline.draw();
             Console.ReadLine();
+            VerticalLine vline = new VerticalLine(8, 13, 10, '+');
+            vline.draw();
         }
         
     }
